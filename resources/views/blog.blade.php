b1a06cdee3b23de276934b9327b5724a679e28d2
Sofa <muhammadsofa52@yahoo.com>
2024-09-16 09:53:42 +0700

    folder structure

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <title>Halaman Blog</title>
+     <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+     <h1>Halaman Blog</h1>
+     <a href="/">Home</a>
+     <a href="/contact">Contact</a>
+     <br>
+     <img src="img/lion.jpg" alt="" width="500">
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Perferendis nesciunt ipsum accusamus laudantium asperiores voluptates voluptatum suscipit non possimus nemo necessitatibus, 
+          eaque repellendus quibusdam eos earum veritatis at. Incidunt, nisi!</p>
+</body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <title>Halaman Conatct</title>
+     <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+     <h1>Halaman Contact</h1>
+     <h2>Nama : {{ $name }}</h2>
+     <p>Instagram: <a href="https://www.instagram.com/muhammad_sofa" target="_blank">muhammad_sofa</a></p>
+</body>
+</html>
