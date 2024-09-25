4bca864f1dad2456428638a8f94c4913d98fc194
Sofa <muhammadsofa52@yahoo.com>
2024-09-26 06:44:30 +0700

    Searching

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,13 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    //$posts = Post::with('author', 'category')->latest()->get();
-    $posts = Post::latest()->get();
-    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
+    $posts = Post::latest();
+
+    if(request('search')) {
+        $posts->where('title', 'like', '%' . request('search') . '%');
+    };
+
+    return view('posts', ['title' => 'Blog', 'posts' => $posts->get()]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
